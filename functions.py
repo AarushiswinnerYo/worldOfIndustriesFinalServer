42337807b0303cc97887dda34c3c3ab8aaf6b257
Aarushiswinner Yo <aarushcanwin@gmail.com>
2026-06-13 11:31:16 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -122,7 +122,7 @@ def getCraftPrices():
 def craftRecipeFunc(token, amount, passwd, materialName, materialSubType=""):
     userNameData=tl.find_one({"token": token})
     username=userNameData['_id']
-    recipe_price=price.find_one({"craftprices":{"$exists":True}}, {'_id':0})["craftprices"][materialName]
+    recipe_price=price.find_one({"craftPrices":{"$exists":True}}, {'_id':0})["craftPrices"][materialName]
     rawMats=rawMaterialToRecipe.find_one({"recipes":{"$exists":True}}, {"_id":0})
     rawNeeds=rawMats["recipes"][materialName]
     materials=rawNeeds.keys()
